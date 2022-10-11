1d286591e4ac99eff21fa478f175a260e99e7dfa
Guadalupe2000V <ve398028@uaeh.edu.mx>
2022-10-11 11:52:26 -0500

    Se agrego el proyecto

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+
+  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_PASS', '' );             // Set database password
+  define( 'DB_NAME', 'bd_inventario_alves' );        // Set database name
+  define( 'DB_PORT', '3310' ); 
+?>
